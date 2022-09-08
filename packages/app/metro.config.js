73dae122c9ca8595fa5f7ced5e88ae7cb35876cd
weiping.xiang <weiping.xiang@shopee.com>
2022-09-08 17:29:29 +0800

    init

---
+++ packages/app/metro.config.js
@@ -0,0 +1,20 @@
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+const path = require('path');
+const findUp = require('find-up');
+
+const projectRoot = process.cwd();
+
+const getRepoRoot = () => {
+  return path.dirname(findUp.sync('pnpm-workspace.yaml', {cwd: process.cwd()}));
+};
+
+module.exports = {
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
+  watchFolders: [
+    path.resolve(projectRoot),
+    path.join(path.resolve(projectRoot), 'node_modules'),
+    getRepoRoot(),
+  ],
+};
